c812fe1bef1b8331759d624bc117a92f9951644d
biren-git <727723eucd011@skcet.ac.in>
2026-07-01 00:10:05 +0530

    Integrate FastAPI backend with scan endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,37 @@
-import json
-
+from fastapi import FastAPI
+from pydantic import BaseModel
 from modules.dns_module import dns_lookup
 from modules.whois_module import whois_lookup
 from modules.ssl_module import ssl_lookup
 
-domain = input("Enter a domain: ")
+app = FastAPI()
+#uvicorn main:app --reload
 
-dns_result = dns_lookup(domain)  
-whois_result = whois_lookup(domain)
-ssl_result = ssl_lookup(domain)
+class ScanRequest(BaseModel):
+    domain:str
 
-result = {
-    "domain": domain,
-    "domainexists": dns_result["domainexists"],
-    "dns": dns_result["dns"],
-    "whois": whois_result,
-    "ssl": ssl_result,
-}
+@app.get("/")
+def home():
+    return {
+      "message":"Welcome to hosthunter API"
+    }
 
-print(json.dumps(result, indent=4, sort_keys=False))
+@app.post("/scan")
+def scan(request : ScanRequest):
+    
+    domain = request.domain
+
+    dns_result = dns_lookup(domain)
+    whois_result = whois_lookup(domain)
+    ssl_result = ssl_lookup(domain)
+
+    result = {
+        "domain" : domain,
+        "domainexists" : dns_result["domainexists"],
+        "dns" : dns_result["dns"],
+        "whois" : whois_result,
+        "ssl" : ssl_result,
+    }
+
+    return result
+    
